92e5570ac38c4967c4107e571237b9ebb5cabbf4
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-17 20:36:55 +0530

    UI changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Header = () => {
-    let Links =[
-      {name:"Home",link:"/"},
-      {name:"About",link:"/"},
-      {name:"Contact",link:"/"}
-    ];
-    let [open,setOpen]=useState(false);
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/" },
+    { name: "Contact", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800'>
-        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-        
-        </span>
-        Mentorship
-      </div>
-      
-      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-      <ion-icon name={open ? 'close':'menu'}></ion-icon>
-      </div>
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+        >
+          <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
+          Mentorship
+        </div>
 
-      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
-        {
-          Links.map((link)=>(
-            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-[1.1rem] cursor-pointer md:hidden"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-500 duration-500"
+              >
+                {link.name}
+              </a>
             </li>
-          ))
-        }
-        <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md px-4 py-2 mx-3">
-              Schedule Appointment
-        </button>
-      </ul>
+          ))}
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md px-4 py-2 mx-3">
+            Schedule Appointment
+          </button>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
