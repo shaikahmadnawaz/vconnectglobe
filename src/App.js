95ab8c741b7dbf7ecb4ca977fa695d992416f21b
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-04-02 10:14:57 +0530

    How do I began my study abroad? Flow chart - 10 points completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Testimonials from "./components/Testimonials";
 import JoinTeam from "./components/JoinTeam";
+import StudyAbroad from "./components/StudyAbroad";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <StudyAbroad />
       <Mentors />
       <Testimonials />
       <JoinTeam />
