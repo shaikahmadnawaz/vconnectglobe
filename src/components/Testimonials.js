00f69c17cbe8be43631fc86537c9ae50aa147400
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-04-02 10:29:43 +0530

    added space between testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -45,36 +45,38 @@ const Testimonials = () => {
         <h2 className="text-2xl text-center font-bold mb-8">
           What people are saying
         </h2>
-        <Slider {...settings}>
-          {testimonials.map((testimonial, index) => {
-            return (
-              <div
-                key={index}
-                className="shadow-md rounded-2xl md:my-5 bg-slate-100 mx-4 p-2 hover:scale-105 duration-500"
-              >
-                <div className="photo flex justify-center py-2 rounded-md">
-                  <img
-                    src="./mentors/cartoon.jpg"
-                    alt="student"
-                    className="rounded-full w-32"
-                  />
+        <div className="space-x-4">
+          <Slider {...settings}>
+            {testimonials.map((testimonial, index) => {
+              return (
+                <div
+                  key={index}
+                  className="scale-75 md:scale-90 shadow-md rounded-2xl md:my-5 bg-slate-100 mx-4 p-2 hover:scale-100 duration-500 mr-4"
+                >
+                  <div className="photo flex justify-center py-2 rounded-md">
+                    <img
+                      src="./mentors/cartoon.jpg"
+                      alt="student"
+                      className="rounded-full w-32"
+                    />
+                  </div>
+                  <div className="flex flex-col mt-4">
+                    <p className="font-bold text-center">{testimonial.name}</p>
+                    <p className="text-gray-500 text-center">
+                      {testimonial.position}
+                    </p>
+                  </div>
+                  <div className="flex italic justify-center px-10 mt-4">
+                    <p>{testimonial.testimonial}</p>
+                  </div>
+                  <div className="mt-4 flex justify-center my-4">
+                    <StarRating rating={testimonial.rating} />
+                  </div>
                 </div>
-                <div className="flex flex-col mt-4">
-                  <p className="font-bold text-center">{testimonial.name}</p>
-                  <p className="text-gray-500 text-center">
-                    {testimonial.position}
-                  </p>
-                </div>
-                <div className="flex italic justify-center px-10 mt-4">
-                  <p>{testimonial.testimonial}</p>
-                </div>
-                <div className="mt-4 flex justify-center my-4">
-                  <StarRating rating={testimonial.rating} />
-                </div>
-              </div>
-            );
-          })}
-        </Slider>
+              );
+            })}
+          </Slider>
+        </div>
       </div>
     </section>
   );
