9e03a6f9e363316583b7a691058c7c9ad6a02439
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-05-13 01:14:53 +0530

    hero section done in both mobile and laptop view

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -42,7 +42,7 @@ function Hero() {
   useEffect(() => {
     const timer = setInterval(() => {
       setActiveHeroIndex((prevIndex) => (prevIndex + 1) % heroData.length);
-    }, 2000);
+    }, 8000);
 
     return () => clearInterval(timer);
   }, []);
@@ -62,37 +62,43 @@ function Hero() {
 
   return (
     <section
-      className={`flex w-full flex-wrap bg-gradient-to-r ${backgroundColor} h-full mx-auto mt-0 pt-12 md:px-14`}
+      className={`flex w-full h-screen flex-wrap bg-gradient-to-r ${backgroundColor} mx-auto mt-0 md:pt-12 md:px-14`}
     >
-      <div className="w-full md:w-1/2">
-        <div
-          id="home"
-          className="container flex justify-center items-center flex-col text-center mx-auto lg:py-40 lg:px-32 pt-24 px-4"
-        >
-          <h1 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-white mb-4">
-            {title}
-          </h1>
-          <p className="text-lg text-white mb-6">{description}</p>
-          <a
-            href={buttonLink}
-            onClick={handleScroll}
-            className="bg-white text-black text-xl py-2 px-3 rounded-xl font-medium hover:bg-black hover:text-white transition-all duration-300"
-          >
-            {buttonText}
-          </a>
-        </div>
-      </div>
-      <div className="w-full md:w-1/2 py-24">
-        <div className="container ml-0 px-4">
-          <img src={image} alt="abroad study mentorship" />
-        </div>
-      </div>
-      <div className="carousel-arrows flex justify-between absolute bottom-0 left-0 right-0 mx-auto mb-4">
+      <div className="flex">
         <button className="arrow prev" onClick={handlePrev}>
-          <BsArrowLeft />
+          <BsArrowLeft className="bg-white ml-2 w-5 md:w-8 md:h-6 rounded-xl" />
         </button>
-        <button className="arrow next" onClick={handleNext}>
-          <BsArrowRight />
+        <div className="flex flex-col md:flex-row mx-8 h-screen">
+          <div className="flex h-full mt-10 md:mt-0 items-center justify-center flex-col w-full md:w-1/2">
+            <div
+              id="home"
+              className="container flex justify-center items-center flex-col text-center mx-auto lg:py-40 lg:px-32 md:pt-24 px-4"
+            >
+              <h1 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-white mb-4">
+                {title}
+              </h1>
+              <p className="text-sm md:text-lg text-white mb-6">
+                {description}
+              </p>
+              <a
+                href={buttonLink}
+                onClick={handleScroll}
+                className="bg-white text-black text-md md:text-xl py-1 md:py-2 px-1 md:px-3 rounded-lg md:rounded-xl font-medium hover:bg-black hover:text-white transition-all duration-300"
+              >
+                {buttonText}
+              </a>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center md:w-1/2 md:py-24">
+            <img
+              src={image}
+              className="w-80 md:w-full mb-10"
+              alt="abroad study mentorship"
+            />
+          </div>
+        </div>
+        <button className="arrow next inline" onClick={handleNext}>
+          <BsArrowRight className="bg-white mr-2 w-5 md:w-8 md:h-6 rounded-xl" />
         </button>
       </div>
     </section>
