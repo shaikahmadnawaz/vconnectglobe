a2c689a053db1d0e524dc789a8632e2a65324faf
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-07 13:11:39 +0530

    testimonial added and making testimonial container good

--- src/Data/testimonialsData.js
+++ src/Data/testimonialsData.js
@@ -3,7 +3,7 @@ export const testimonials = [
     name: "Mr. Prasad",
     university: "University of New Haven",
     testimonial:
-      "I was hesitant to study abroad, but this mentorship program made the entire process so much easier. The mentor I worked with was patient, supportive, and encouraging. They helped me identify my goals and find a program that was the perfect fit for me. I had an incredible experience studying abroad and would do it all over again in a heartbeat!",
+      "The mentorship program made studying abroad easier. My mentor was patient, supportive, and helped me find the perfect program. I had an incredible experience and would do it all again in a heartbeat!",
     rating: 5,
     mentorName: "Mr.Srinivasa Reddy",
     image: "./testimonials_photos/prasad.jpg",
@@ -12,7 +12,7 @@ export const testimonials = [
     name: "Ms. Prithee",
     university: "University of Houston",
     testimonial:
-      "Very informative session. Insights are so specific to the requirement and the guidance is good, all the queries are resolved. Would definitely recommend to take a notes of questions prior to the session clear all the doubts. They help to give even after support to clear also unsliced queries. This session is recommended to the people who are seeing information related to studies, part time , assistantships and any personal queries like safety such. Thank you Maheshwari for giving the required information.",
+      "The session was informative and specific to my requirements. The guidance provided was helpful and all my queries were resolved. I would recommend taking notes of questions prior to the session for clarity. Maheshwari provided support even after the session for any remaining questions. Thank you for the valuable information.",
     rating: 4,
     mentorName: "Ms.Maheswari",
     image: "./testimonials_photos/preethi.jpg",
@@ -21,7 +21,7 @@ export const testimonials = [
     name: "Ms. Lalitha",
     university: "University of New Haven",
     testimonial:
-      "My experience with the session was  amazing! He has taught me valuable information and given me a great advice for my masters  . I am extremely grateful to this opportunity and the knowledge I have gained . As a result I feel  more comfortable to clear all doubts about my assistantships, part-times and as well as course work . Thank you Avinash Reddy for giving the grateful knowledge and information .",
+      "The session was amazing and provided valuable information for my masters. I am grateful for the opportunity and the knowledge gained. I feel more confident about assistantships, part-time work, and coursework. Thank you, Avinash Reddy, for the insightful guidance.",
     rating: 5,
     mentorName: "Mr.Avinash Reddy ",
     image: "./testimonials_photos/lalitha.jpg",
@@ -31,9 +31,18 @@ export const testimonials = [
     name: "Mr. Pavana Chary",
     university: "University of Houston",
     testimonial:
-      "I wanted to take a moment to express my gratitude for the time you've spent with me discussing college and courses. Your explanations have been incredibly helpful and have provided me with a deeper understanding of the enrollment process.I also appreciate the way you have patiently answered all of my queries. Your responses have been clear and concise, and have helped to alleviate any concerns I had.I feel much more confident about starting my abroad masters journey thanks to your guidance.",
+      "I am grateful for the time you spent discussing college and courses with me. Your explanations were incredibly helpful and provided a deeper understanding of the enrollment process. Thank you for patiently answering all my queries and alleviating my concerns. Your guidance has boosted my confidence for my abroad masters journey.",
     rating: 5,
     mentorName: "Mr.Srinivasa Reddy",
     image: "./testimonials_photos/pavanachary.png",
+  },
+  {
+    name: "Mr. Tajuddin",
+    university: "Illinois Institute of Technology",
+    testimonial:
+      "They provided valuable information about universities, program suitability, and fees. They assisted in finding a bank consultant for a loan without collateral. Mr. Akheel explained top priority colleges and job prospects in construction management. They covered travel, visa, expenses, accommodations, and safety comprehensively.",
+    rating: 5,
+    mentorName: "Mr. Akheel",
+    image: "./testimonials_photos/tajuddin-taj.jpg",
   },
 ];
